c7b3116a9ec56b8def75a74ecfbd6f141a661e16
Logan Olson <jloganolson@gmail.com>
2021-03-12 09:13:54 -0800

    moving reporter into the then statement

--- index.js
+++ index.js
@@ -12,6 +12,30 @@ async function main() {
     const globber = await glob.create('**/*index.html');
     const files = await globber.glob();
 
+    const rulePageLang = new Rule({
+      name: 'Language of Page',
+  
+      message: 'Please add the lang attribute to the HTML tag',
+  
+      ruleUrl:
+        'https://www.w3.org/TR/UNDERSTANDING-WCAG20/meaning-doc-lang-id.html',
+  
+      level: 'A',
+  
+      template: false,
+  
+      callback: function (dom) {
+        var lang = dom.$('html')[0].lang;
+        console.log({lang, isThere: typeof lang === 'undefined' || lang === '' })
+        if (typeof lang === 'undefined' || lang === '') {
+          throw {
+            reportType: 'error',
+            el: dom.$('html')[0],
+          };
+        }
+      },
+    });
+
     const ruleImageAlt = new Rule({
       name: 'All img must have alt',
 
@@ -36,41 +60,16 @@ async function main() {
       },
     });
 
-    // console.log(files[0]);
     const reporter = new Reporter();
     JSDOM.fromFile(files[0]).then((dom) => {
-      console.log(dom);
       dom.window.$ = $(dom.window);
       ruleImageAlt.applyRule(dom.window, reporter);
+      rulePageLang.applyRule(dom.window, reporter);
     });
-    // const dom = await new JSDOM.fromFile(files[0]);
-    // rulePageLang.applyRule(dom.window, reporter);
-    console.log(reporter);
+    
+    
   } catch (error) {
     core.setFailed(error.message);
   }
 }
 main();
-
-// const rulePageLang = new Rule({
-//   name: 'Language of Page',
-
-//   message: 'Please add the lang attribute to the HTML tag',
-
-//   ruleUrl:
-//     'https://www.w3.org/TR/UNDERSTANDING-WCAG20/meaning-doc-lang-id.html',
-
-//   level: 'A',
-
-//   template: false,
-
-//   callback: function (dom, reporter) {
-//     var lang = dom.$('html').attr('lang');
-//     if (typeof lang === 'undefined' || lang === '') {
-//       throw {
-//         reportType: 'error',
-//         el: dom.$('html').parent().html(),
-//       };
-//     }
-//   },
-// });
